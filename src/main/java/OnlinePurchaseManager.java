c398d14f27d38143c462bdc366411755d2f314e3
KYCHU1 <142847721+KYCHU1@users.noreply.github.com>
2024-04-24 21:41:17 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -13,11 +13,9 @@ public class OnlinePurchaseManager
   {
     /* to be implemented in part (a) */
     int count = 0;
-    for(int i = 0; i <purchases.size(); i++){
-      if(purchases.get(i).getMaker() == maker && purchases.get(i).isElectronic == true){
-        count++;
-      }
-    }
+    for(Gizmo g:purchases)
+      if(g.isElectronic() && g.getMaker().equals(maker))
+        num++;
     return count;
   }
   
